12577fe56490967b8b55d2ab8c639bf382b870c6
Cole Ma <acromema@icloud.com>
2019-01-16 19:46:00 +0800

    添加真分数题目模块

---
+++ PFG/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using PFGen;
+
+namespace PFG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Problem P = new Problem();
+           Console.WriteLine(P.problem); 
+           PF result = P.CalValue();
+           result.Display();
+        //    PF p1 = new PF();
+        //    p1.Display();
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     P = new Problem();
+            //     result = P.CalValue();
+            //     Console.WriteLine(P.problem);
+            //     result.Display();
+            //     Console.Write("\n");
+            // }  
+        }
+    }
+}
